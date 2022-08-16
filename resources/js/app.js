0764d2869826075e334bb7de0aa2a9fe11c3d3fc
hugoybrahim <ybra72@gmail.com>
2022-08-16 12:10:12 -0400

    breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,23 @@
 import './bootstrap';
-const boton = document.querySelector('#botonnav');
-const menu = document.querySelector('#menu');
+import '../css/app.css';
 
-boton.addEventListener('click', () => {
-    console.log('click')
-    menu.classList.toggle('hidden')
-})
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const btn = document.querySelectorAll('#btnnotemenu');
-const drop = document.querySelectorAll('#notemenu');
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
-btn.forEach((element,key) => {
-    element.addEventListener('click', () => {
-        console.log('click')
-        drop[key].classList.toggle('hidden')
-    })
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
 });
+
+InertiaProgress.init({ color: '#4B5563' });
